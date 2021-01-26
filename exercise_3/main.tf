22dd0cb22556c9c6c8c9ec28f5077d99bfa4c2e3
Gabriel Cerioni <gacerioni@gmail.com>
2021-01-26 10:31:39 -0300

    Update main.tf

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -1,27 +1,26 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
 }
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
-   region = "us-east-1"
+   bucket = "gabs-remote-tf-state-bucket"
+   region = "us-east-2"
   }
 }
 
 resource "aws_s3_bucket" "tf-root-module-bucket" {
-  bucket = "${var.s3_bucket_name}"
+  bucket = var.s3_bucket_name
   acl = "private"
-  region = "${var.s3_bucket_region}"
   
   versioning {
     enabled = true
   }
   
   tags = {
-    Name        = "${var.s3_bucket_name}"
-    Environment = "${var.tag_env}"
+    Name        = var.s3_bucket_name}
+    Environment = var.tag_env
   }
 }
