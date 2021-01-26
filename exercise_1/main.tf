e33e14eb1de536da9361429a63eaaf5cd1e22515
Gabriel Cerioni <gacerioni@gmail.com>
2021-01-25 21:21:19 -0300

    upgrading TF syntax

--- exercise_1/main.tf
+++ exercise_1/main.tf
@@ -1,21 +1,21 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
 }
 
 resource "aws_s3_bucket" "tf-root-module-bucket" {
-  bucket = "${var.s3_bucket_name}"
+  bucket = var.s3_bucket_name
   acl = "private"
-  region = "${var.s3_bucket_region}"
+  region = var.s3_bucket_region
   
   versioning {
     enabled = true
   }
   
   tags = {
-    Name        = "${var.s3_bucket_name}"
-    Environment = "${var.tag_env}"
+    Name        = var.s3_bucket_name}
+    Environment = var.tag_env
   }
  }
 ## module "s3_module_private_repo" {
